6189b73ecb23e89cb14323c5a82d92466f597130
Sidney Swift <158200036+sidneyswift@users.noreply.github.com>
2024-09-29 09:49:39 -0400

    Configure external image domain for next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
+          pathname: '/**',  // This allows all paths from this hostname
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
