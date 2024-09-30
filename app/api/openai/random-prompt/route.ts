da3a2575b6edf2c9a86a47365f77d14d2dfe66d0
Sidney Swift <158200036+sidneyswift@users.noreply.github.com>
2024-09-30 14:07:50 -0400

    I add next js cache fix

--- app/api/openai/random-prompt/route.ts
+++ app/api/openai/random-prompt/route.ts
@@ -1,3 +1,8 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
+export const runtime = 'edge';
+
 import { OpenAI } from 'openai';
 import { NextResponse } from 'next/server';
 
